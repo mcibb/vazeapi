42c5b029b8c62765f3359c4021038073c7d8360c
mcibb <marian.cibulka222@gmail.com>
2022-01-16 18:20:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ var express = require("express");
 
 var app = express();
 
-var server = app.listen(process.env.PORT || 5000, listening);
+const host = "0.0.0.0";
+const port = process.env.PORT || 3000;
+app.listen(port, host, listening);
 
 function listening() {
     console.log("listening");
