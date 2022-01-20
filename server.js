dc5885d8ba1e190c3f160496ad374d72065b4f04
mcibb <marian.cibulka222@gmail.com>
2022-01-21 00:18:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ function addText(request, response) {
     
    
     
-    let write = JSON.stringify(db, null, 2);
+    let write = JSON.stringify(activities, null, 2);
     fs.writeFile("activities.json", write, finished);
 
     function finished(err) {
