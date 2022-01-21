2a0cd3b66c9aeb85d6af2914677db96c00687bdb
mcibb <marian.cibulka222@gmail.com>
2022-01-21 17:31:21 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -166,7 +166,7 @@ async function getData(month, the){
 
 async function retDay(lines, line, month, the, hour) {
 
-    let ret = getData(month, the);
+    let ret = await getData(month, the);
     let fin = ret[hour];
     console.log(fin);
     
